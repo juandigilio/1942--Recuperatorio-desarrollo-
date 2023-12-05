0b866545f384b13a99a942eb42437c059c0e80b2
juandigilio <juanignaciodigilio@gmail.com>
2023-12-04 21:15:54 -0300

    Credits added

---
+++ Reversioned 1942/Src/Instructions.cpp
@@ -0,0 +1,91 @@
+#include "Instructions.h"
+
+#include "raylib.h"
+
+#include "Menu.h"
+
+using namespace Menu;
+
+namespace Instructions
+{
+	static void GetInput(GameSceen& currentSceen)
+	{
+		int mouseX = GetMouseX();
+		int mouseY = GetMouseY();
+
+		if (IsKeyPressed(KEY_ESCAPE))
+		{
+			currentSceen = GameSceen::MENU;
+		}
+		else if ((mouseX > backButtonPos.x && mouseX < backButtonPos.x + backButton.width) && (mouseY > backButtonPos.y && mouseY < backButtonPos.y + backButton.height))
+		{
+			DrawTextureV(backButtonAct, backButtonPos, WHITE);
+
+			if (!isClicking)
+			{
+				isClicking = true;
+
+				PlaySound(click);
+			}
+
+			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+			{
+				PlaySound(clickPressed);
+				currentSceen = GameSceen::MENU;
+			}
+		}
+	}
+
+	static void DrawInstrucions()
+	{
+		bigWindowPos.x = (screenWidth / 2.0f) - (bigWindow.width / 2.0f);
+		bigWindowPos.y = (screenHeight / 2.0f) - (bigWindow.height / 2.0f);
+
+		Vector2 textPos;
+
+		DrawTexture(background, 0, 0, WHITE);
+		DrawTextureV(bigWindow, bigWindowPos, WHITE);
+		DrawTextureV(backButton, backButtonPos, WHITE);
+
+		textPos.x = (screenWidth / 2) - MeasureTextEx(font, "INSTRUCTIONS", fontSize * 0.85f, spacing / 2.0f).x / 2.0f;
+		textPos.y = (screenHeight / 6) - fontSize + 10;
+		DrawTextEx(font, "INSTRUCTIONS", textPos, fontSize * 0.85f, spacing / 2.0f, RAYWHITE);
+
+		textPos.x = (screenWidth / 2) - MeasureTextEx(font, "Press left click once to shoot and get pressed", fontSize / 4.0f, spacing / 4.0f).x / 2.0f;
+		textPos.y = (screenHeight / 5) + (fontSize)-20;
+		DrawTextEx(font, "Press once left click to shoot and get pressed", textPos, fontSize / 4.0f, spacing / 4.0f, RAYWHITE);
+
+		textPos.x = (screenWidth / 2) - MeasureTextEx(font, "right click to acelerate, ship will allways follow", fontSize / 4.0f, spacing / 4.0f).x / 2.0f;
+		textPos.y += 40;
+		DrawTextEx(font, "right click to acelerate, ship will allways follow", textPos, fontSize / 4.0f, spacing / 4.0f, RAYWHITE);
+
+		textPos.x = (screenWidth / 2) - MeasureTextEx(font, "mouse arrow. Destroy as many asteroids as possible", fontSize / 4.0f, spacing / 4.0f).x / 2.0f;
+		textPos.y += 40;
+		DrawTextEx(font, "mouse arrow. Destroy as many asteroids as possible", textPos, fontSize / 4.0f, spacing / 4.0f, RAYWHITE);
+
+		textPos.x = (screenWidth / 2) - MeasureTextEx(font, "and set the new record! Game will end when you", fontSize / 4.0f, spacing / 4.0f).x / 2.0f;
+		textPos.y += 40;
+		DrawTextEx(font, "and set the new record! Game will end when you", textPos, fontSize / 4.0f, spacing / 4.0f, RAYWHITE);
+
+		textPos.x = (screenWidth / 2) - MeasureTextEx(font, "loose all your lives, but there is not a win", fontSize / 4.0f, spacing / 4.0f).x / 2.0f;
+		textPos.y += 40;
+		DrawTextEx(font, "loose all your lives, but there is not a win", textPos, fontSize / 4.0f, spacing / 4.0f, RAYWHITE);
+
+		textPos.x = (screenWidth / 2) - MeasureTextEx(font, "condition, it's just a highest score comp.", fontSize / 4.0f, spacing / 4.0f).x / 2.0f;
+		textPos.y += 40;
+		DrawTextEx(font, "condition, it's just a highest score comp.", textPos, fontSize / 4.0f, spacing / 4.0f, RAYWHITE);
+
+		textPos.x = (screenWidth / 2) - MeasureTextEx(font, "(Press ESC to pause)", fontSize / 3.0f, spacing / 3.0f).x / 2.0f;
+		textPos.y += 160;
+		DrawTextEx(font, "(Press ESC to pause)", textPos, fontSize / 3.0f, spacing / 3.0f, RAYWHITE);
+	}
+
+	void ShowInstructions(GameSceen& currentSceen)
+	{
+		DrawInstrucions();
+
+		UpdateMusicStream(menuMusic);
+
+		GetInput(currentSceen);
+	}
+}
